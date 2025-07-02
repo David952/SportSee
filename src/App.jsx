ef2d7a49e415290454cce17d6dbaa03d3cb11ee2
David952 <david.amorim@live.fr>
2025-07-02 19:14:47 +0200

    Mise à jour de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import StatCards from "./components/StatCards.jsx";
+import DailyActivity from "./components/DailyActivity.jsx";
+import AverageSessions from "./components/AverageSessions.jsx";
+import Performance from "./components/Performance.jsx";
+import Score from "./components/Score.jsx";
 
 import { useEffect, useState } from "react";
-import { fetchUserById } from "./services/apiMock.js";
+import {
+	fetchUserById,
+	fetchUserActivityById,
+	fetchUserAverageSessionsById,
+	fetchUserPerformanceById,
+} from "./services/apiMock.js";
 
 function App() {
 	const [user, setUser] = useState(null);
+	const [activity, setActivity] = useState(null);
+	const [averageSessions, setAverageSessions] = useState(null);
+	const [performance, setPerformance] = useState(null);
 
 	useEffect(() => {
 		fetchUserById(1).then(setUser);
+		fetchUserActivityById(1).then(setActivity);
+		fetchUserAverageSessionsById(1).then(setAverageSessions);
+		fetchUserPerformanceById(1).then((data) => setPerformance(data));
 	}, []);
 
 	return (
@@ -27,6 +42,33 @@ function App() {
 						Félicitations ! Vous avez explosé vos objectifs hier 👏
 					</p>
 					<div className="flex">
+						<div className="flex flex-col mr-8">
+							<div className="bg-[#FBFBFB] h-[320px] w-[835px] mb-7">
+								<h2>Activité quotidienne</h2>
+								{activity ? <DailyActivity activity={activity} /> : "Chargement..."}
+							</div>
+							<div className="flex justify-between">
+								<div className="bg-[#FBFBFB] h-[263px] w-[258px]">
+									<h2>Durée moyenne des sessions</h2>
+									{averageSessions ? (
+										<AverageSessions averageSessions={averageSessions} />
+									) : (
+										"Chargement..."
+									)}
+								</div>
+								<div className="bg-[#FBFBFB] h-[263px] w-[258px]">
+									{performance ? (
+										<Performance data={performance.data.data} kind={performance.data.kind} />
+									) : (
+										"Chargement..."
+									)}
+								</div>
+								<div className="bg-[#FBFBFB] h-[263px] w-[258px]">
+									<h2>Score</h2>
+									{user ? <Score score={user.data.todayScore} /> : "Chargement..."}
+								</div>
+							</div>
+						</div>
 						{user && (
 							<StatCards
 								data={{
