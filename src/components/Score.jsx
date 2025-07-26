ede90d3dc227fde42882dbac1146bcdd329fbbda
David952 <david.amorim@live.fr>
2025-07-26 10:54:57 +0200

    Mise à jour de Score.jsx

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,8 +4,13 @@ function Score({ score }) {
 	const data = [
 		{
 			name: "Score",
-			value: score * 100,
+			value: score,
 			fill: "#FF0000",
+		},
+		{
+			name: "",
+			value: 1,
+			fill: "transparent",
 		},
 	];
 
@@ -14,20 +19,18 @@ function Score({ score }) {
 			<h2 className="absolute start-0 top-4 left-5 font-medium">Score</h2>
 			<ResponsiveContainer width="100%" height="100%">
 				<RadialBarChart
-					cx="50%"
-					cy="50%"
 					innerRadius="70%"
 					outerRadius="80%"
 					barSize={10}
 					data={data}
-					startAngle={85}
-					endAngle={220}
+					startAngle={90}
+					endAngle={450}
 				>
 					<RadialBar minAngle={15} background clockWise dataKey="value" cornerRadius={10} />
 				</RadialBarChart>
 			</ResponsiveContainer>
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
-				<p className="text-2xl font-bold">{data[0].value}%</p>
+				<p className="text-2xl font-bold">{data[0].value * 100}%</p>
 				<p className="text-base text-grey">de votre objectif</p>
 			</div>
 		</div>
